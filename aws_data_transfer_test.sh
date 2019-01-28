858b3ef79d7e1f7bce5c11741afdead1fb618ca7
Jason Bramwell <jb2cool@gmail.com>
2019-01-28 13:38:31 +0000

    Improving comments

--- aws_data_transfer_test.sh
+++ aws_data_transfer_test.sh
@@ -17,10 +17,10 @@
     file_size=50
 
     # Set file location to put sample data
-    file_location_root=~/AWS_data_test				# We will make this on the fly
-    file_location_a=$file_location_root\/a			# We will make this on the fly
-    file_location_b=$file_location_root\/b			# We will make this on the fly
-    file_location_local_scratch=$file_location_root\/scratch	# We will make this on the fly
+    file_location_root=~/AWS_data_test			# We will make this on the fly
+    file_location_a=$file_location_root\/a		# We will make this on the fly
+    file_location_b=$file_location_root\/b		# We will make this on the fly
+    file_location_scratch=$file_location_root\/scratch	# We will make this on the fly
     file_name=file_test
 
     # Set number of files to create
@@ -28,7 +28,7 @@
 
     # Times to perform operation
     iterations=10
-    iterations_minus_1=$(echo $(( $iterations - 1 )))		# This is just to 
+    iterations_minus_1=$(echo $(( $iterations - 1 )))	# This is just to correct the number of repeats
 
 
 # The workflow starts here
@@ -36,8 +36,8 @@
 # Tell user what we are going to do
 echo "We will create $file_number files $file_size MiB in size here $file_location_a."
 echo "We will them move these files to $file_location_b."
-echo "We will create a scratch folder here $file_location_local_scratch."
-echo "We will download the files from $file_location_b to $file_location_local_scratch."
+echo "We will create a scratch folder here $file_location_scratch."
+echo "We will download the files from $file_location_b to $file_location_scratch."
 echo "We will then clean everything up before repeating this $iterations_minus_1 times."
 echo "If this is not what you want to do press Ctrl-C and edit the script to change functionality."
 read -p "If this is what you want press return to continue."
@@ -48,7 +48,7 @@ do
  echo "Creating directories"
  mkdir -p $file_location_a
  mkdir -p $file_location_b
- mkdir -p $file_location_local_scratch 
+ mkdir -p $file_location_scratch 
  
  # Create some test data in file location A
  for i in $(seq 1 $file_number);
@@ -61,17 +61,17 @@ do
  echo "Moving data files from A to B"
  mv -v $file_location_a\/$file_name* $file_location_b
 
- # Download test data from file location B to a local scratch folder
+ # Download test data from file location B to scratch folder
  echo "Copying data files from B to scratch"
- cp -v $file_location_b\/$file_name* $file_location_local_scratch
+ cp -v $file_location_b\/$file_name* $file_location_scratch
 
  # Delete test data
  echo "Cleaning up data files"
  rm -vr $file_location_b\/$file_name*
- rm -vr $file_location_local_scratch\/*
+ rm -vr $file_location_scratch\/*
  rmdir -v $file_location_a
  echo "Cleaning up directories"
  rmdir -v $file_location_b
- rmdir -v $file_location_local_scratch
+ rmdir -v $file_location_scratch
  rmdir -v $file_location_root
 done
