0f2ecbdfc56d1522d876f64d5f2587dda59802e6
Jason Bramwell <jb2cool@gmail.com>
2019-01-28 11:26:58 +0000

    Automated directory creation

--- aws_data_transfer_test.sh
+++ aws_data_transfer_test.sh
@@ -14,12 +14,13 @@
 # Variables (This is the only bit that needs user configuration)
 
     # Set file size in megabytes (This is more user friendly than using bytes)
-    file_size=10
+    file_size=50
 
     # Set file location to put sample data
-    file_location_a=~/a # This should be an existing folder
-    file_location_b=~/b # This should be an existing folder
-    file_location_local_scratch=~/scratch # We will make this on the fly
+    file_location_root=~/AWS_data_test # We will make this on the fly
+    file_location_a=$file_location_root\/a # We will make this on the fly
+    file_location_b=$file_location_root\/b # We will make this on the fly
+    file_location_local_scratch=$file_location_root\/scratch # We will make this on the fly
     file_name=file_test
 
     # Set number of files to create
@@ -43,21 +44,34 @@ read -p "If this is what you want press return to continue."
 
 for i in $(seq 1 $repeats);
 do
+ # Create directories
+ echo "Creating directories"
+ mkdir -p $file_location_a
+ mkdir -p $file_location_b
+ mkdir -p $file_location_local_scratch 
+ 
  # Create some test data in file location A
  for i in $(seq 1 $file_number);
  do
+  echo "Creating dummy data files"
   dd if=/dev/urandom of=$file_location_a\/$file_name\_$i count=$file_size bs=1M;
  done
 
  # Move test data from file location A to file location B
+ echo "Moving data files from A to B"
  mv -v $file_location_a\/$file_name* $file_location_b
 
  # Download test data from file location B to a local scratch folder
- mkdir $file_location_local_scratch
+ echo "Copying data files from B to scratch"
  cp -v $file_location_b\/$file_name* $file_location_local_scratch
 
  # Delete test data
+ echo "Cleaning up data files"
  rm -vr $file_location_b\/$file_name*
  rm -vr $file_location_local_scratch\/*
+ rmdir -v $file_location_a
+ echo "Cleaning up directories"
+ rmdir -v $file_location_b
  rmdir -v $file_location_local_scratch
+ rmdir -v $file_location_root
 done
