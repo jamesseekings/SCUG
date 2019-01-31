456f6b9e1a90464565f07e2de382ff8ab48f4dd8
Jason Bramwell <jb2cool@gmail.com>
2019-01-31 12:12:10 +0000

    Cleaning up variables and improving readability

--- s3_file_transfer_test.sh
+++ s3_file_transfer_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#######################################################################################
-# Amazon data transfer test
-#
-# This script creates some dummy data files in one file location (file_location_a)
-# It then moves these files to a second file location (file_location_b)
-# It'll then create a scratch folder (file_location_local_scratch) for download tests
-# Then download these files from file_location_b to the local scratch folder
-# Then clean everything up before repeating itself (if desired)
-#######################################################################################
+#################################################################################
+# S3 file transfer test								#
+#										#
+# This script sets up some temp directories to work in				#
+# It then creates some dummy data in one file location (file_location_dd)	#
+# It then moves these files to an S3 bucket (file_location_s3)			#
+# Then downloads these files from file_location_s3 to the local scratch folder	#
+# Then clean everything up before repeating itself (if desired)			#
+#################################################################################
 
 # Clear the screen to reduce clutter
 clear
@@ -21,18 +21,18 @@ read -p "How large (In mebibytes) do you want your file(s) to be? " file_size
 
 # Set file location to put sample data
 read -p "Where would you like the temporary data directories created (Ideally locally)? " -e -i ~/s3-file-transfer-test file_location_root
-file_location_a=$file_location_root\/a								# We will make this on the fly
-read -p "Where is your remote storage? " -e -i s3:\/\/s3-file-transfer-test file_location_s3	# We will make this on the fly
+file_location_dd=$file_location_root\/dd							# We will make this on the fly
+read -p "Where is your remote storage? " file_location_s3
 file_location_scratch=$file_location_root\/scratch						# We will make this on the fly
 file_name=file_test
 
 # Tell user what we are going to do
 echo ""
 echo "We will create some directories to work in:"
-echo " $file_location_a"
+echo " $file_location_dd"
 echo " $file_location_scratch"
 echo ""
-echo "We will create $file_number file(s) $file_size MiB in size here $file_location_a."
+echo "We will create $file_number file(s) $file_size MiB in size here $file_location_dd."
 echo ""
 echo "We will move the file(s) to $file_location_s3."
 echo "This simulates writing to remote location."
@@ -48,21 +48,21 @@ for i in $(seq 1 $iterations);
 do
  # Create directories
  echo "Creating directories"
- mkdir -p $file_location_a
+ mkdir -p $file_location_dd
  mkdir -p $file_location_scratch
 
- # Create some test data in file location A
+ # Create some test data in file location data
  for i in $(seq 1 $file_number);
  do
   echo "Creating dummy data files"
-  dd if=/dev/urandom of=$file_location_a\/$file_name\_$i count=$file_size bs=1M;
+  dd if=/dev/urandom of=$file_location_dd\/$file_name\_$i count=$file_size bs=1M;
  done
 
- # Move test data from file location A to file location B
- echo "Moving data files from A to S3"
- aws s3 mv $file_location_a $file_location_s3 --recursive --exclude "*" --include "$file_name*"
+ # Move test data from file location dd to file location S3
+ echo "Moving data files from DD to S3"
+ aws s3 mv $file_location_dd $file_location_s3 --recursive --exclude "*" --include "$file_name*"
 
- # Download test data from file location B to scratch folder
+ # Download test data from S3 to scratch folder
  echo "Copying data files from S3 to scratch"
  aws s3 cp $file_location_s3 $file_location_scratch --recursive --exclude "*" --include "$file_name*"
 
@@ -70,7 +70,7 @@ do
  echo "Cleaning up data files"
  aws s3 rm $file_location_s3 --recursive --exclude "*" --include "$file_name*"
  rm -vr $file_location_scratch\/*
- rmdir -v $file_location_a
+ rmdir -v $file_location_dd
  echo "Cleaning up directories"
  rmdir -v $file_location_scratch
  rmdir -v $file_location_root
